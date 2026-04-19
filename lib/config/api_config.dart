6687f0d84ffd580ce753f3fab53b090bf2ef21c9
muhamadairul <akunpertamapublic@gmail.com>
2026-04-19 20:42:54 +0700

    feat: add ApiConfig class to centralize backend API endpoints

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'https://61b3-203-190-43-197.ngrok-free.app/api';
+  static const String baseUrl = 'https://personal-finance-backend-one.vercel.app/api';
 
   // Auth endpoints
   static const String login = '/login';
